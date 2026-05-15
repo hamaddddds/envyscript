c752bd813f886678c05b066a1f24e6886f259fb2
hamaddddds <kanekiken11409@gmail.com>
2026-05-15 17:52:40 +0700

    fix: move game routing into jnkieloader to bypass Vercel cache, hide Jnkie URLs

--- jnkieloader.lua
+++ jnkieloader.lua
@@ -3,6 +3,16 @@ Junkie.service = "EnvyHub"
 Junkie.identifier = "1091392"
 Junkie.provider = "Envy"
 
+if getgenv().EnvyLoaded then 
+    warn("[ENVY] Script is already running!")
+    return 
+end
+getgenv().EnvyLoaded = true
+
+print("Game finded")
+print("Executing : Loading...")
+task.wait(0.5)
+
 local function verifyKey()
     local key = getgenv().SCRIPT_KEY
     
@@ -26,7 +36,29 @@ local function verifyKey()
 end
 
 if verifyKey() then
-    if getgenv().MAIN_SCRIPT_URL then
+    local PlaceId = game.PlaceId
+
+    -- Game Mappings (private - hidden inside Jnkie obfuscation)
+    local gameScripts = {
+        [3351674303]     = "62519ca922088c8b14de3119e243f7a419eef043a6cdc30184a16a96f1f32e11", -- Driving Empire
+        [4901815153]     = "62519ca922088c8b14de3119e243f7a419eef043a6cdc30184a16a96f1f32e11", -- Driving Empire (Alt)
+        [131378148336503] = "c5d362d6d9949216afa44bd6c765ddac95fcab11482049635ffb3d93a2412e00", -- DDS
+        [89469502395769]  = "6ccb179ff34595f749f0853ea83b9710378e10d53a1ab6038e6bd9071d80b9c0", -- Kick ALB
+        [8356562067]      = "71c2a27557c6a9c58a14c4c54d640e121b91cf1395c377ba95391c2011a591d7", -- Indo Voice
+        [6911148748]      = "d878315dcdabb13eb31727b7c1cf521a480fc5a60d29de01a82cc777107a2a90", -- CDID Main
+        [110369730911937]  = "d878315dcdabb13eb31727b7c1cf521a480fc5a60d29de01a82cc777107a2a90", -- CDID Jatim Map
+    }
+
+    local scriptHash = gameScripts[PlaceId]
+
+    if scriptHash then
+        local url = "https://api.jnkie.com/api/v1/luascripts/public/" .. scriptHash .. "/download"
+        loadstring(game:HttpGet(url))()
+    elseif getgenv().MAIN_SCRIPT_URL then
+        -- Fallback: load from MAIN_SCRIPT_URL if set (for future games)
         loadstring(game:HttpGet(getgenv().MAIN_SCRIPT_URL))()
+    else
+        warn("[ENVY] Game not supported! (ID: " .. tostring(PlaceId) .. ")")
+        game.Players.LocalPlayer:Kick("[ENVY] This game is not supported.")
     end
 end
